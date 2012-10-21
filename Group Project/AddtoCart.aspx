4b6ecf239247ce380bda18f7f12e183c864c4ab7
Christiaan Burger <christiaan.burger@hotmail.com>
2012-10-21 19:55:19 +0200

    Database
    Products added to the database by Mr Demba

---
+++ Group Project/AddtoCart.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="AddtoCart.aspx.vb" Inherits="Group_Project.AddtoCart" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            height: 25px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:Label ID="lblfail" runat="server" Text="Invalid product ID" 
+        ForeColor="Red" BorderStyle="Dotted" Visible="False"></asp:Label>
+    <div id="cart" runat="server">
+    <table border="1">
+        <tr>
+        <td align="center" class="style1"><div id="namediv" runat="server">Product Name Here </div></td>
+        <td class="style1"><div id="imagediv" runat="server">Product Image goes here</div></td>
+        <td class="style1"><div id="pricediv" runat="server">Product Price Goes here</div></td>
+        <td class="style1"><div id="taxdiv" runat="server">Product Tax Goes here</div></td>
+        <td class="style1"><div id="stockdiv" runat="server">Availeble Stock Goes Here</div></td>
+        </tr>
+        
+        <tr>
+        <td width="250">
+            <asp:Label ID="lblAdd" runat="server" Text="Stock/Success" 
+                Visible="False" BorderStyle="Dotted"></asp:Label></td>
+        <td></td>
+        <td width="150">
+            <asp:Label ID="lblAmount" runat="server" Text="Specify The amount you wish to buy"></asp:Label>
+            </td>
+        <td>
+            <asp:TextBox ID="txtNum" runat="server"></asp:TextBox>
+            </td>
+        <td>
+            <asp:Button ID="cmdAdd" runat="server" Text="Add to Cart" />
+        </td>
+        </tr>
+    </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Group Project/AddtoCart.aspx.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class AddtoCart
+    Inherits System.Web.UI.Page
+    Private itemID As String
+    Private connect As SqlConnection
+    Private sql As String
+    Private command As SqlCommand
+    Private dr As SqlDataReader
+    Private constring As String = "Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|Products.mdf;Integrated Security=True;User Instance=True"
+    Private Stock As Integer
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        itemID = Request.QueryString("ProductID")
+        sql = "select * from Products Where ProductID='" & itemID & "';"
+        connect = New SqlConnection(constring)
+        command = New SqlCommand(sql)
+        command.CommandType = CommandType.Text
+        command.Connection = connect
+        command.Connection.Open()
+        command.ExecuteNonQuery()
+        dr = command.ExecuteReader(CommandBehavior.CloseConnection)
+
+        If dr.HasRows Then
+            dr.Read()
+            namediv.InnerHtml = dr("Name")
+            setImage(dr("Image"))
+            getprice(dr("Price"))
+            Stock = CInt(dr("Stock"))
+            stockdiv.InnerHtml = Stock
+            If Stock = 0 Then
+                stockdiv.InnerHtml &= " Currently out of stock"
+            End If
+        Else
+            lblfail.Visible = True
+            cmdAdd.Enabled = False
+            txtNum.Enabled = False
+        End If
+        dr.Close()
+        command.Connection.Dispose()
+        command.Dispose()
+
+    End Sub
+
+    Private Sub getprice(ByRef p As String)
+        Dim price As Double = CDbl(p)
+        Dim tax As Double = price * 0.14
+        pricediv.InnerHtml = "R" & price & " inc VAT"
+        taxdiv.InnerHtml = "R" & tax
+    End Sub
+
+    Private Sub setImage(ByRef i As String)
+        Dim idir As String = ".\Images\" & i
+        imagediv.InnerHtml = "<img src='" & idir & "' width=300 hight=300>Immage</img>"
+    End Sub
+
+    Protected Sub cmdAdd_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdAdd.Click
+        Dim requestedstock As Integer = CInt(txtNum.Text)
+        If requestedstock <= 0 Then
+            lblAmount.Text = "Invalid Stock Amount Specified: incorrect value in text box"
+            lblAmount.ForeColor = Drawing.Color.Red
+            lblAmount.Visible = True
+            Return
+        ElseIf requestedstock > Stock Then
+            lblAmount.Text = "Invalid Stock Amount Specified: you may not order more items then there is stock"
+            lblAmount.ForeColor = Drawing.Color.Red
+            lblAmount.Visible = True
+            Return
+        End If
+
+        'we now know we can continue with transaction
+        'for now we asume that the Transactios table is in the same database
+        Dim username As String = Session("UserID")
+        sql = "Insert Into Transactions Values ('" & username & "', '" & itemID & "', " & requestedstock & ");"
+        connect = New SqlConnection(constring)
+        command = New SqlCommand(sql)
+        command.CommandType = CommandType.Text
+        command.Connection = connect
+        command.Connection.Open()
+        command.ExecuteNonQuery()
+        Stock = Stock - requestedstock
+        stockdiv.InnerHtml = Stock
+        sql = "Update Products Set Stock=" & Stock & " Where ProductID='" & itemID & "';"
+        command.CommandText = sql
+        command.ExecuteNonQuery()
+
+        lblAmount.Text = "Product Added successfully"
+        lblAmount.ForeColor = Drawing.Color.Green
+        lblAmount.Visible = True
+
+        command.Connection.Close()
+        command.Connection.Dispose()
+        command.Dispose()
+
+    End Sub
+End Class
